e8ceb41879348e3ec5e30be4cb21032668840dd6
Astrid Aguilar <mixaguilar@yahoo.com>
2021-05-19 21:18:59 +0000

    created tweets and styled tweeter feed with html/css yesterday (+ tried to implement timeAgo), in the process of making tweets dynamic

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,59 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+// Render Tweets //
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+}
+
+
+// Create Tweet Element //
+const createTweetElement = function (tweetObject) {
+  let $tweet;
+
+
+  return $tweet;
+};
+
+renderTweets(data);
+
+
+// TIMEAGO base //
+$(function () {
+  console.log("ready!");
+  const tweetTime = $("#tweet-time");
+  const time = tweetTime.data("time");
+  console.log(time);
+  const timeAgoTime = timeago.format(time);
+  tweetTime.append(timeAgoTime);
+});
+
