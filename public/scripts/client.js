92206329e40d4ed528113c6e3fb0d8d530dca6c4
Astrid Aguilar <mixaguilar@yahoo.com>
2021-05-20 18:58:29 +0000

    fetches tweets with AJAX, and fixed the form validation to get character counter to work more efficiently

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,63 +6,43 @@
 
 $(document).ready(function() {
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  // {
-  //   "user": {
-  //     "name": "Descartes",
-  //     "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //     "handle": "@rd"
-  //   },
-  //   "content": {
-  //     "text": "Je pense , donc je suis"
-  //   },
-  //   "created_at": 1461113959088
-  // }
-];
 
 // Render Tweets //
 const renderTweets = function (tweets) {
 
-  for (let tweet of tweets) {
-    $(".tweets-container").append(createTweetElement(tweet));
-  }
+  $("#tweet-container").empty();
+  for (const tweet of tweets) {
+    $(".tweets-container").prepend(createTweetElement(tweet));
+  };
 
 };
 
 // Create Tweet Element //
 const createTweetElement = function (tweet) {
+  const profileName = tweet.user.name;
+  const profilePic = tweet.user.avatars;
+  const profileHandle = tweet.user.handle;
+  const tweetContent = tweet.content.text;
+  const tweetTime = tweet.created_at;
 
   let $header = `
       <header>
         <div class="profile">
-          <img src="${tweet.user.avatars}">
-          <span class="display-name">${tweet.user.name}</span>
+          <img src="${profilePic}">
+          <span class="display-name">${profileName}</span>
         </div>
-          <span class="user-name">${tweet.user.handle}</span>
+          <span class="user-name">${profileHandle}</span>
       </header>
         `
 
   let $tweeted = `
     <div class="text-tweet">
-      <p>${tweet.content.text}</p>
+      <p>${tweetContent}</p>
       </div>`
 
   let $footer = `
     <footer class="tweet-footer">
-      <span data-time="${tweet.created_at}" id="tweet-time" class="time-since-tweet"></span>
+      <span data-time="${tweetTime}" id="tweet-time" class="time-since-tweet"></span>
         <div class="tweet-icons">
           <i class="fas fa-flag"></i>
           <i class="fas fa-retweet"></i>
@@ -78,33 +58,45 @@ const createTweetElement = function (tweet) {
     </article>
   `;
 
-  // let $tweeted = `
-  //   <article class="tweets-container">
-  //     <header>
-  //       <div class="profile">
-  //         <img src="${tweet.user.avatars}">
-  //         <span class="display-name">${tweet.user.name}</span>
-  //       </div>
-  //         <span class="user-name">${tweet.user.handle}</span>
-  //     </header>
-  //     <div class="text-tweet">
-  //       <p>${tweet.content.text}</p>
-  //      </div>
-  //        <footer class="tweet-footer">
-  //          <span data-time="${tweet.created_at}" id="tweet-time" class="time-since-tweet"></span>
-  //            <div class="tweet-icons">
-  //              <i class="fas fa-flag"></i>
-  //              <i class="fas fa-retweet"></i>
-  //              <i class="fas fa-heart"></i>
-  //            </div>
-  //        </footer>
-  // `
-
-  // return $tweeted;
-
 };
 
-renderTweets(data);
+$("#tweet-post").submit(function(event) {
+  const textData = ($(this).serialize());
+  const textRemaining = $("#tweet-text").val();
+  event.preventDefault();
+
+
+  // FORM VALIDATION //
+  if (textRemaining.length > 140) {
+    alert("You've exceeded the maximum of characters for a tweet!");
+    return;
+  }
+
+  if (textRemaining === "" || textRemaining === null) {
+    alert("Your tweet is empty!")
+  }
+
+  $.post("/tweets", textData)
+  .then(loadTweets)
+  $("#tweet-text").val("");
+  
+});
+
+const loadTweets = function() {
+  $.ajax({
+    method: "GET",
+    url: "/tweets",
+    dataType: "json",
+    success: function(data) {
+      renderTweets(data);
+    },
+    error: function() {
+      alert("Tweets not found!");
+    }
+  });
+};
+
+loadTweets();
 
 });
 
