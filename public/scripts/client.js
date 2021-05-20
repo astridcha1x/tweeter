d3c9bee3c05c0986e37b828b1d8e82cd6341a44e
Astrid Aguilar <mixaguilar@yahoo.com>
2021-05-20 02:10:23 +0000

    dynamic tweets implication - added functions to client.js to render the tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
 // Fake data taken from initial-tweets.json
 const data = [
   {
@@ -18,37 +20,93 @@ const data = [
     },
     "created_at": 1461116232227
   },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+  // {
+  //   "user": {
+  //     "name": "Descartes",
+  //     "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //     "handle": "@rd"
+  //   },
+  //   "content": {
+  //     "text": "Je pense , donc je suis"
+  //   },
+  //   "created_at": 1461113959088
+  // }
+];
 
 // Render Tweets //
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-}
+const renderTweets = function (tweets) {
 
+  for (let tweet of tweets) {
+    $(".tweets-container").append(createTweetElement(tweet));
+  }
+
+};
 
 // Create Tweet Element //
-const createTweetElement = function (tweetObject) {
-  let $tweet;
+const createTweetElement = function (tweet) {
 
+  let $header = `
+      <header>
+        <div class="profile">
+          <img src="${tweet.user.avatars}">
+          <span class="display-name">${tweet.user.name}</span>
+        </div>
+          <span class="user-name">${tweet.user.handle}</span>
+      </header>
+        `
 
-  return $tweet;
+  let $tweeted = `
+    <div class="text-tweet">
+      <p>${tweet.content.text}</p>
+      </div>`
+
+  let $footer = `
+    <footer class="tweet-footer">
+      <span data-time="${tweet.created_at}" id="tweet-time" class="time-since-tweet"></span>
+        <div class="tweet-icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+    </footer>`
+
+  return `
+    <article>
+      ${$header}
+      ${$tweeted}
+      ${$footer}
+    </article>
+  `;
+
+  // let $tweeted = `
+  //   <article class="tweets-container">
+  //     <header>
+  //       <div class="profile">
+  //         <img src="${tweet.user.avatars}">
+  //         <span class="display-name">${tweet.user.name}</span>
+  //       </div>
+  //         <span class="user-name">${tweet.user.handle}</span>
+  //     </header>
+  //     <div class="text-tweet">
+  //       <p>${tweet.content.text}</p>
+  //      </div>
+  //        <footer class="tweet-footer">
+  //          <span data-time="${tweet.created_at}" id="tweet-time" class="time-since-tweet"></span>
+  //            <div class="tweet-icons">
+  //              <i class="fas fa-flag"></i>
+  //              <i class="fas fa-retweet"></i>
+  //              <i class="fas fa-heart"></i>
+  //            </div>
+  //        </footer>
+  // `
+
+  // return $tweeted;
+
 };
 
 renderTweets(data);
 
+});
 
 // TIMEAGO base //
 $(function () {
